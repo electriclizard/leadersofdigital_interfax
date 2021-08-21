130c24d86e6654854a6d270eba3adee8a556c5c1
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2021-08-21 12:59:08 +0500

    feed added with json

--- app.py
+++ app.py
@@ -3,13 +3,23 @@
 
 from typing import Container
 import dash
+from dash.dependencies import Output, State, Input
+from dash_bootstrap_components._components.Collapse import Collapse
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash_html_components.Tr import Tr
 import pandas as pd
+import json
+import re
+
+with open('dataset_public.json') as inputJSON:
+    data = json.load(inputJSON)
+    inputJSON.close
+
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.BOOTSTRAP])
+                external_stylesheets=[dbc.themes.BOOTSTRAP], title='Interfax')
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -22,11 +32,47 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Admin", href="/admin")),
 
     ],
-
     brand="Home",
     brand_href="/",
     color="primary",
     dark=True,)
+
+
+def make_news(news, num):
+    return dbc.Card(
+        [
+            dbc.CardHeader(
+
+                dbc.Button(
+                    news['headline'],
+                    # id=str(num)+'button',
+                    n_clicks=0,
+                    color="link",
+                    style={'textAlign': 'left'})
+            ),
+
+
+        ]
+        # +
+        # [dbc.Collapse(
+
+        #     dbc.CardBody(cluster['news'][0]['body']),
+        #     id=str(num),
+        #     is_open=False
+        # )]
+    )
+    pass
+
+
+def make_cluster(cluster, num):
+    return dbc.Card([
+        dbc.CardHeader(html.H4(cluster['title'])),
+        dbc.CardBody(
+            [
+                    make_news(cluster['news'][i], i) for i in range(len(cluster['news']))
+                    ]
+        )])
+
 
 index_page = html.Div([
     navbar,
@@ -35,10 +81,29 @@ index_page = html.Div([
         html.Div('Здесь расположены карточки со статьями'),
         html.Div('Сортировка карточек от новой к старой'),
         html.Div('в карточке открыта новейшая и закрыты старые статьи'),
-        html.Div('Заголовок карточки - тематика')
-    ])
+        html.Div('Заголовок карточки - тематика'),
 
+    ]+[make_cluster(data[i], i) for i in range(100)])
 ])
+
+
+# @app.callback(
+#     [Output(str(i), 'is_open') for i in range(100)],
+#     [Input(str(i)+'button', 'n_clicks')for i in range(100)],
+#     [State(str(i), 'is_open')for i in range(100)]
+# )
+# def toogle_news(*args):
+#     ctx = dash.callback_context
+
+#     if not ctx.triggered:
+#         return [False for i in range(100)]
+#     else:
+#         button_id = re.match(r'\d+',ctx.triggered[0]["prop_id"].split(".")[0])[0]
+
+#     stateList = list(args[int(len(args)/2):])
+#     stateList[int(button_id)]= not stateList[int(button_id)]
+#     return stateList
+
 
 adminPanel = html.Div([
 
@@ -59,8 +124,6 @@ adminPanel = html.Div([
             "На этой странице должен быть выбор статьи или кластера",
             html.Br(),
             "После выбора будет предложено название темы?"]),
-
-
     ])
 
 ])
