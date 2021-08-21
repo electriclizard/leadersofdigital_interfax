8ca9381434450e99fcee9eef772fc36f5401d0ed
Artur <iar@napoleonit.ru>
2021-08-21 15:45:45 +0500

    tfidf, ngram to handlers

--- handlers/header_generation.py
+++ handlers/header_generation.py
@@ -1,17 +1,25 @@
-def create_header_service():
+from service.header_generator import InterfaxHeaderCreator
+
+
+def create_dummy_header_service():
     from infrastructure.creator_model.abstract import DummyModel
-    from service.header_generator import InterfaxHeaderCreator
 
     header_model = DummyModel()
     header_service = InterfaxHeaderCreator(header_model)
     return header_service
 
 
-handlers = {
-    "recognize_picture": create_header_service()
-}
+def create_ngram_service():
+    from infrastructure.creator_model.models import NgrammModel
+
+    header_model = NgrammModel()
+    header_service = InterfaxHeaderCreator(header_model)
+    return header_service
 
 
-def get_service(service_name: str):
-    handler = handlers.get(service_name, None)
-    return handler
+def create_tfidf_service():
+    from infrastructure.creator_model.models import TfidfModel
+
+    header_model = TfidfModel()
+    header_service = InterfaxHeaderCreator(header_model)
+    return header_service
