61aa1198c1242d2912ae112acd1b3712c1085d13
Artur <iar@napoleonit.ru>
2021-08-21 15:45:25 +0500

    tfidf and ngram generation infrastructure

---
+++ infrastructure/creator_model/models.py
@@ -0,0 +1,48 @@
+import re
+from typing import List, Tuple
+from collections import Counter
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from infrastructure.creator_model.abstract import BaseModel
+
+
+class NgrammModel(BaseModel):
+
+    def __init__(self):
+        self.ngram_range = (3, 6)
+
+    def inference_model(self, model_input=List[str]) -> List:
+        most_popular_headers = self.most_popular_ngram(
+            news=model_input,
+            ngram_range=self.ngram_range
+        )
+        return most_popular_headers
+
+    @staticmethod
+    def most_popular_ngram(news: List[str], ngram_range: Tuple) -> List:
+        counter = Counter()
+        for n in news:
+            tokens = [token for token in re.findall(r'\w+', n)]
+            for ngram in range(ngram_range[0], ngram_range[-1]+1):
+                for i in range(len(tokens)-ngram):
+                    counter[' '.join(tokens[i:i+ngram])] += 1
+        return counter.most_common()[:20]
+
+
+class TfidfModel(BaseModel):
+
+    def __init__(self, ngram_range=(3, 4), max_features=10000):
+        self.tfidf = TfidfVectorizer(ngram_range=(3, 4), max_features=100000).fit(None)
+        pass
+
+    def inference_model(self, model_input=List[str]):
+        generated_headers = self.tfidf_generate(model_input)
+        return generated_headers
+
+    def tfidf_generate(self, news: List[str]) -> List:
+        indexes = (-self.tfidf.transform(news).sum(axis=0)).argsort()[0, :20].tolist()[0]
+        res = []
+        for i in indexes:
+            res.append(self.tfidf.get_feature_names()[i])
+        return res
